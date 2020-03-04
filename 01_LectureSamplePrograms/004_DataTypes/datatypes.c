5afc6193a119561efa647a7c558e457ca31f7e7f
Adrian Koller <adrian.koller@hslu.ch>
2020-03-04 10:48:22 +0100

    changed folder name to match convention

---
+++ 01_LectureSamplePrograms/004_DataTypes/datatypes.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){
+
+	printf("Printing data type sizes... \n");
+
+	printf("Size of char: %ld\n", sizeof(char));
+
+
+	return 0;
+}
---
+++ 01_LectureSamplePrograms/005ifelse/ifelse.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+
+	int inputValue1 = 0;
+	int inputValue2 = 0;
+
+
+	return 0;
+}
